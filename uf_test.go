0a8fcb2d0f8f6a7ff14e1a0738018dbef83587c7
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-04-13 13:38:27 -0600

    union find 100% test coverage

---
+++ uf_test.go
@@ -0,0 +1,72 @@
+package uf
+
+import (
+	"testing"
+)
+
+func TestInterface(t *testing.T) {
+	t.Parallel()
+	u := New(5)
+	data := [][]int{
+		{0, 1},
+		{1, 2},
+		{2, 3},
+		{3, 4},
+	}
+	for _, pair := range data {
+		p, q := pair[0], pair[1]
+		if !u.Connected(p, q) {
+			u.Union(p, q)
+		}
+	}
+	if c := u.Count(); c != 1 {
+		t.Errorf("Expected 1 got %d", c)
+	}
+	if u.Find(0) != u.Find(1) ||
+		u.Find(1) != u.Find(2) ||
+		u.Find(2) != u.Find(3) ||
+		u.Find(3) != u.Find(4) {
+		t.Errorf("all of vertices should be joined now")
+	}
+	u.Union(0, 1)
+	u.Union(1, 2)
+	u.Union(2, 3)
+	u.Union(3, 4)
+	if u.Find(0) != u.Find(1) ||
+		u.Find(1) != u.Find(2) ||
+		u.Find(2) != u.Find(3) ||
+		u.Find(3) != u.Find(4) {
+		t.Errorf("all of vertices should still be joined now")
+	}
+}
+
+func TestInterfaceReversed(t *testing.T) {
+	t.Parallel()
+	u := New(5)
+	data := [][]int{
+		{4, 3},
+		{3, 2},
+		{1, 2},
+		{1, 0},
+	}
+	for _, pair := range data {
+		p, q := pair[0], pair[1]
+		if !u.Connected(p, q) {
+			u.Union(p, q)
+		}
+	}
+	if c := u.Count(); c != 1 {
+		t.Errorf("Expected 1 got %d", c)
+	}
+}
+
+func TestInterfacePanic(t *testing.T) {
+	t.Parallel()
+	u := New(5)
+	defer func() {
+		if err := recover(); err == nil {
+			t.Errorf("Expected a panic")
+		}
+	}()
+	u.Find(5)
+}
